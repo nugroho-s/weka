5119526cb4dfbbbaaacb37088336d33c122a75cb
nugroho-s <massatriya@gmail.com>
2016-10-31 12:29:09 +0700

    init

---
+++ src/Weka/Weka.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Weka;
+
+/**
+ *
+ * @author user
+ */
+public class Weka {
+    public static void main (String[] args){
+        System.out.println("hello world");
+    }
+}
