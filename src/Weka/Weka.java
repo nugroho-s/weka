3fe758504a761a4ad2340d79cfa9431adf6e0258
nugroho-s <massatriya@gmail.com>
2016-11-01 13:54:19 +0700

    delete useless function

--- src/Weka/Weka.java
+++ src/Weka/Weka.java
@@ -9,10 +9,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instances;
 import weka.filters.Filter;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.filters.supervised.attribute.Discretize;
 
 /*ahihi
@@ -30,102 +32,6 @@ public class Weka {
     public static Instances data;
     public static Instances newData;
     
-    public static void contoh_tulis ()throws Exception{
-        ArrayList<Attribute>	atts;
-        ArrayList<Attribute>	attsRel;
-        ArrayList<String>		attVals;
-        ArrayList<String>		attValsRel;
-        Instances			data;
-        Instances			dataRel;
-        double[]			vals;
-        double[]			valsRel;
-        int				i;
-
-        // 1. set up attributes
-        atts = new ArrayList<Attribute>();
-        // - numeric
-        atts.add(new Attribute("att1"));
-        // - nominal
-        attVals = new ArrayList<String>();
-        for (i = 0; i < 5; i++)
-          attVals.add("val" + (i+1));
-        atts.add(new Attribute("att2", attVals));
-        // - string
-        atts.add(new Attribute("att3", (ArrayList<String>) null));
-        // - date
-        atts.add(new Attribute("att4", "yyyy-MM-dd"));
-        // - relational
-        attsRel = new ArrayList<Attribute>();
-        // -- numeric
-        attsRel.add(new Attribute("att5.1"));
-        // -- nominal
-        attValsRel = new ArrayList<String>();
-        for (i = 0; i < 5; i++)
-          attValsRel.add("val5." + (i+1));
-        attsRel.add(new Attribute("att5.2", attValsRel));
-        dataRel = new Instances("att5", attsRel, 0);
-        atts.add(new Attribute("att5", dataRel, 0));
-
-        // 2. create Instances object
-        data = new Instances("MyRelation", atts, 0);
-
-        // 3. fill with data
-        // first instance
-        vals = new double[data.numAttributes()];
-        // - numeric
-        vals[0] = Math.PI;
-        // - nominal
-        vals[1] = attVals.indexOf("val3");
-        // - string
-        vals[2] = data.attribute(2).addStringValue("This is a string!");
-        // - date
-        vals[3] = data.attribute(3).parseDate("2001-11-09");
-        // - relational
-        dataRel = new Instances(data.attribute(4).relation(), 0);
-        // -- first instance
-        valsRel = new double[2];
-        valsRel[0] = Math.PI + 1;
-        valsRel[1] = attValsRel.indexOf("val5.3");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        // -- second instance
-        valsRel = new double[2];
-        valsRel[0] = Math.PI + 2;
-        valsRel[1] = attValsRel.indexOf("val5.2");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        vals[4] = data.attribute(4).addRelation(dataRel);
-        // add
-        data.add(new DenseInstance(1.0, vals));
-
-        // second instance
-        vals = new double[data.numAttributes()];  // important: needs NEW array!
-        // - numeric
-        vals[0] = Math.E;
-        // - nominal
-        vals[1] = attVals.indexOf("val1");
-        // - string
-        vals[2] = data.attribute(2).addStringValue("And another one!");
-        // - date
-        vals[3] = data.attribute(3).parseDate("2000-12-01");
-        // - relational
-        dataRel = new Instances(data.attribute(4).relation(), 0);
-        // -- first instance
-        valsRel = new double[2];
-        valsRel[0] = Math.E + 1;
-        valsRel[1] = attValsRel.indexOf("val5.4");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        // -- second instance
-        valsRel = new double[2];
-        valsRel[0] = Math.E + 2;
-        valsRel[1] = attValsRel.indexOf("val5.1");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        vals[4] = data.attribute(4).addRelation(dataRel);
-        // add
-        data.add(new DenseInstance(1.0, vals));
-
-        // 4. output data
-        System.out.println(data);
-    }
-    
     public static void baca(String file)throws Exception{
         BufferedReader reader = new BufferedReader(new FileReader(file));
         data = new Instances(reader);
@@ -135,9 +41,11 @@ public class Weka {
     }
     
     public static Instances filter(Instances x) throws Exception{
-        String[] options = new String[2];
+        String[] options = new String[4];
         options[0] = "-R";                                    // "range"
-        options[1] = "1";                                     // first attribute
+        options[1] = "first-last";                                     // first attribute
+        options[2] = "-precision";
+        options[3] = "6";
         Discretize dis = new Discretize();
         dis.setOptions(options);
         dis.setInputFormat(x);
@@ -145,12 +53,18 @@ public class Weka {
     }
     
     public static void main(String[] args){
+        Scanner r = new Scanner(System.in);
         try{
             baca("data/iris.arff");
             System.out.println(data);
             System.out.println("=================");
-            newData = filter(data);
-            System.out.println(newData);
+            char filter;
+            System.out.println("Ingin menggunakan filter? (Y/N)");
+            filter = (char) System.in.read();
+            if (Character.toLowerCase(filter)=='y'){
+                newData = filter(data);
+                System.out.println(newData);
+            }
         } catch (Exception e){
             System.out.println(e);
         }
