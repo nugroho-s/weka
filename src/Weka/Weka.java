71131a7550315d057249b3dca8b53de93772b183
nugroho-s <massatriya@gmail.com>
2016-11-02 13:24:40 +0700

    add tambah instance

--- src/Weka/Weka.java
+++ src/Weka/Weka.java
@@ -19,6 +19,7 @@ import weka.filters.Filter;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.filters.supervised.attribute.Discretize;
 import weka.classifiers.Evaluation;
+import weka.core.Instance;
 
 /*ahihi
 */
@@ -31,6 +32,7 @@ public class Weka {
     public static Instances data;
     public static Classifier cls;
     public static Evaluation eval;
+    public static Scanner r = new Scanner(System.in);
     
     public static void baca(String file)throws Exception{
         BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -71,37 +73,70 @@ public class Weka {
         return temp;
     }
     
+    public static void select_train() throws Exception{
+        System.out.print("Masukkan metode training ft(full training)/cv(10 folds cross-validation): ");
+        String train_mode = r.next();
+        System.out.println(cls);
+        if(train_mode.equals("ft")){
+            //training dengan metode full training
+            eval = full_training(data,cls);
+            System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+        }
+    }
+    
     public static void main(String[] args){
-        Scanner r = new Scanner(System.in);
         try{
+            ArrayList<Attribute> atts = new ArrayList<Attribute>();
+            ArrayList<String> classVal = new ArrayList<String>();
+            classVal.add("Iris-setosa");
+            classVal.add("Iris-versicolor");
+            classVal.add("Iris-virginica");
+            atts.add(new Attribute("sepallength"));
+            atts.add(new Attribute("sepalwidth"));
+            atts.add(new Attribute("petallength"));
+            atts.add(new Attribute("petalwidth"));
+            atts.add(new Attribute("@@class@@", classVal));
             baca(lokasi_file);
             System.out.println(data);
+            System.out.println(data.toSummaryString());
             System.out.println("=================");
-            char filter;
-            System.out.println("Ingin menggunakan filter? (Y/N)");
-            filter = (char) System.in.read();
-            if (Character.toLowerCase(filter)=='y'){
-                data = filter(data);
-                System.out.println(data);
-            }
             System.out.print("Masukkan nama model di folder model (- jika belum ada model): ");
             String model =  r.next();
             if ("-".equals(model)){
                 //tanpa model, save masuk sini
+                char filter;
+                System.out.print("Ingin menggunakan filter? (Y/N)");
+                filter = (char) System.in.read();
+                if (Character.toLowerCase(filter)=='y'){
+                    data = filter(data);
+                    System.out.println(data);
+                }
                 classification(data);
                 cls = classification(data);
-                System.out.print("Masukkan metode training ft(full training)/cv(10 folds cross-validation): ");
-                String train_mode = r.next();
-                System.out.println(cls);
-                if(train_mode.equals("ft")){
-                    //training dengan metode full training
-                    eval = full_training(data,cls);
-                    System.out.println(eval.toSummaryString("\nResults\n======\n", false));
-                }
+                select_train();
             } else {
                 //dengan model, load masuk sini
+                char filter;
+                System.out.print("Gunakan filter? (Y/N): ");
+                filter = (char) System.in.read();
+                if (Character.toLowerCase(filter)=='y')
+                    data = filter(data);
                 cls = load_model(model);
                 System.out.println(cls);
+                select_train();
+            }
+            System.out.println("Ingin mengklasifikasi data? (Y/N): ");
+            char new_data = (char) System.in.read();
+            if (Character.toLowerCase(new_data)=='y'){
+                double[] attValues = new double[data.numAttributes()];
+                for (int i=0;i<data.numAttributes()-1;i++){
+                    attValues[i] = r.nextDouble();
+                }
+                Instance i1 = new DenseInstance(1.0, attValues);
+                i1.setDataset(data);
+                cls.classifyInstance(i1);
+                System.out.println(i1);
+                
             }
         } catch (Exception e){
             System.out.println(e);
